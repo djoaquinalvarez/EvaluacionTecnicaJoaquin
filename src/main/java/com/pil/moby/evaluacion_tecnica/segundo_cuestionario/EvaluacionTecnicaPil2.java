b5dc50424957eac7daa7647696294e49b81ce194
Joaquin Alvarez <alvarezdjoaquin@gmail.com>
2023-01-03 13:31:30 -0300

    Reorganizacion de codigo

--- src/main/java/com/pil/moby/evaluacion_tecnica/segundo_cuestionario/EvaluacionTecnicaPil2.java
+++ src/main/java/com/pil/moby/evaluacion_tecnica/segundo_cuestionario/EvaluacionTecnicaPil2.java
@@ -50,6 +50,62 @@ public class EvaluacionTecnicaPil2 {
 
     }
 
+    private static List<Candidato> inicializarCandidatos() {
+        // TODO: Realizar implementación.
+        List<Candidato> candidatos = new ArrayList<>();
+        Long[] idsCandidatos = {3L,7L,1L,4L,5L,2L,10L,9L,8L,6L};
+        String[] nombresCandidatos = {"Jhon","Matias","Joaquin","Gaston","Pablo","Lucas","Lionel","Julian","Lula","Agustin"};
+        String[] apellidosCandidatos = {"Doe","Otamendi","Tagliafico","Mac Allister","De Paul","Di Maria","Messi","Alvarez","Martinez","Dybala"};
+        Double[] pretencionSalarial = {5000D,3000D,2000D,5000.50D,9000.50D,6000D,10000.10D,5000D,1000D,1000D};
+        Integer[] aniosExperiencia = {2,9,6,1,4,7,10,3,2,2};
+
+        //listadoTecnologias = inicializarTecnologias();
+        Tecnologia[][] tecnologiasCandidatos = {
+                {buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre("Angular"), buscarTecnologiaPorNombre("SQL")},
+                {buscarTecnologiaPorNombre("NodeJS"), buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre("SQL")},
+                {buscarTecnologiaPorNombre("MongoDB"), buscarTecnologiaPorNombre("NoSQL")},
+                {buscarTecnologiaPorNombre("Vue"), buscarTecnologiaPorNombre("SQL"), buscarTecnologiaPorNombre("Java")},
+                {buscarTecnologiaPorNombre("Java")},
+                {buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre("Angular"), buscarTecnologiaPorNombre("SQL")},
+                {buscarTecnologiaPorNombre("SQL"), buscarTecnologiaPorNombre("Vue"), buscarTecnologiaPorNombre("Java")},
+                {buscarTecnologiaPorNombre("NoSQL"), buscarTecnologiaPorNombre("MongoDB"), buscarTecnologiaPorNombre("SQL"), buscarTecnologiaPorNombre("Java")},
+                {buscarTecnologiaPorNombre(".NET"), buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre("Angular")},
+                {buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre(".NET"), buscarTecnologiaPorNombre("Angular")}
+        };
+
+        for (int i = 0; i < idsCandidatos.length; i++) {
+            candidatos.add(new Candidato(
+                    idsCandidatos[i],
+                    nombresCandidatos[i],
+                    apellidosCandidatos[i],
+                    aniosExperiencia[i],
+                    pretencionSalarial[i],
+                    Arrays.stream(tecnologiasCandidatos[i]).toList()
+            ));
+        }
+        return candidatos;
+    }
+
+
+    private static List<Tecnologia> inicializarTecnologias() {
+        List<Tecnologia> tecnologias = new ArrayList<>();
+        Long[] idsTecnologia = {1L,2L,3L,4L,5L,6L,7L,8L};
+        String[] nombresTecnologia = {"Java","Angular","SQL","NoSQL","Vue",".NET","MongoDB","NodeJS"};
+
+        for (int i = 0; i < idsTecnologia.length; i++){
+            tecnologias.add(new Tecnologia(idsTecnologia[i], nombresTecnologia[i]));
+        }
+        return tecnologias;
+    }
+
+
+    private static Tecnologia buscarTecnologiaPorNombre(String nombre) {
+        Optional<Tecnologia> optionalTecnologia = listadoTecnologias.stream()
+                .filter(t -> t.getNombre().equals(nombre))
+                .findFirst();
+        return optionalTecnologia.orElse(null);
+    }
+
     private static void resolverPunto1() {
         List<Candidato> candidatos = inicializarCandidatos();
         //candidatos.forEach(System.out::println);
@@ -150,62 +206,6 @@ public class EvaluacionTecnicaPil2 {
                 return Integer.compare(cantidadTecnologiasCandidato1.compareTo(cantidadTecnologiasCandidato2), 0);
             }
         };
-    }
-
-    private static List<Candidato> inicializarCandidatos() {
-        // TODO: Realizar implementación.
-        List<Candidato> candidatos = new ArrayList<>();
-        Long[] idsCandidatos = {3L,7L,1L,4L,5L,2L,10L,9L,8L,6L};
-        String[] nombresCandidatos = {"Jhon","Matias","Joaquin","Gaston","Pablo","Lucas","Lionel","Julian","Lula","Agustin"};
-        String[] apellidosCandidatos = {"Doe","Otamendi","Tagliafico","Mac Allister","De Paul","Di Maria","Messi","Alvarez","Martinez","Dybala"};
-        Double[] pretencionSalarial = {5000D,3000D,2000D,5000.50D,9000.50D,6000D,10000.10D,5000D,1000D,1000D};
-        Integer[] aniosExperiencia = {2,9,6,1,4,7,10,3,2,2};
-
-        //listadoTecnologias = inicializarTecnologias();
-        Tecnologia[][] tecnologiasCandidatos = {
-                {buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre("Angular"), buscarTecnologiaPorNombre("SQL")},
-                {buscarTecnologiaPorNombre("NodeJS"), buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre("SQL")},
-                {buscarTecnologiaPorNombre("MongoDB"), buscarTecnologiaPorNombre("NoSQL")},
-                {buscarTecnologiaPorNombre("Vue"), buscarTecnologiaPorNombre("SQL"), buscarTecnologiaPorNombre("Java")},
-                {buscarTecnologiaPorNombre("Java")},
-                {buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre("Angular"), buscarTecnologiaPorNombre("SQL")},
-                {buscarTecnologiaPorNombre("SQL"), buscarTecnologiaPorNombre("Vue"), buscarTecnologiaPorNombre("Java")},
-                {buscarTecnologiaPorNombre("NoSQL"), buscarTecnologiaPorNombre("MongoDB"), buscarTecnologiaPorNombre("SQL"), buscarTecnologiaPorNombre("Java")},
-                {buscarTecnologiaPorNombre(".NET"), buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre("Angular")},
-                {buscarTecnologiaPorNombre("Java"), buscarTecnologiaPorNombre(".NET"), buscarTecnologiaPorNombre("Angular")}
-        };
-
-        for (int i = 0; i < idsCandidatos.length; i++) {
-            candidatos.add(new Candidato(
-                    idsCandidatos[i],
-                    nombresCandidatos[i],
-                    apellidosCandidatos[i],
-                    aniosExperiencia[i],
-                    pretencionSalarial[i],
-                    Arrays.stream(tecnologiasCandidatos[i]).toList()
-            ));
-        }
-        return candidatos;
-    }
-
-
-    private static List<Tecnologia> inicializarTecnologias() {
-        List<Tecnologia> tecnologias = new ArrayList<>();
-        Long[] idsTecnologia = {1L,2L,3L,4L,5L,6L,7L,8L};
-        String[] nombresTecnologia = {"Java","Angular","SQL","NoSQL","Vue",".NET","MongoDB","NodeJS"};
-
-        for (int i = 0; i < idsTecnologia.length; i++){
-            tecnologias.add(new Tecnologia(idsTecnologia[i], nombresTecnologia[i]));
-        }
-        return tecnologias;
-    }
-
-
-    private static Tecnologia buscarTecnologiaPorNombre(String nombre) {
-        Optional<Tecnologia> optionalTecnologia = listadoTecnologias.stream()
-                .filter(t -> t.getNombre().equals(nombre))
-                .findFirst();
-        return optionalTecnologia.orElse(null);
     }
 
     /**
